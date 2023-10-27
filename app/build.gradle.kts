9ecddd8d57cd29e7a6b3e66a859ba37deecb307f
JOO0507 <wlduswn12@gmail.com>
2023-10-27 15:10:50 +0900

    1027 3:10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,8 @@ plugins {
 
 android {
     namespace = "com.example.tableorder"
-    compileSdk = 33
+    compileSdk = 34
+    viewBinding { enable = true }
 
     defaultConfig {
         applicationId = "com.example.tableorder"

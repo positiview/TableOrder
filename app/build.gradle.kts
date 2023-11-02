bfda8d95f5044d84d1ff87f18d3a3c8dac9f04f0
wothd0406 <wothd0406@gmail.com>
2023-11-02 16:44:23 +0900

    no message

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,9 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:18.0.1")
     implementation("com.google.android.gms:play-services-location:19.0.1")
 
+    //카드뷰 디펜던시 추가
+    implementation("androidx.cardview:cardview:1.0.0")
+
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
--- app/src/main/java/com/example/mytableorder/adapter/MyFragmentStateAdapter.kt
+++ app/src/main/java/com/example/mytableorder/adapter/MyFragmentStateAdapter.kt
@@ -3,9 +3,6 @@ package com.example.mytableorder.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.example.mytableorder.fragment.OneFragment
-import com.example.mytableorder.fragment.ThreeFragment
-import com.example.mytableorder.fragment.TwoFragment
 
 
 // Adapter는 데이터를 RecyclerView에 주입하는 객체

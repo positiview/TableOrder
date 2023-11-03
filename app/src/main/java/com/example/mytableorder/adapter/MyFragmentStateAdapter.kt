5b081ae7e56fd8f21ae519fe60261ceafb1feeda
JOO0507 <wlduswn12@gmail.com>
2023-11-03 09:44:00 +0900

    리셋

--- app/src/main/java/com/example/mytableorder/adapter/MyFragmentStateAdapter.kt
+++ app/src/main/java/com/example/mytableorder/adapter/MyFragmentStateAdapter.kt
@@ -3,10 +3,9 @@ package com.example.mytableorder.adapter
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-<<<<<<< HEAD
-=======
+
 import com.example.mytableorder.fragment.home.HomeFragment
->>>>>>> e176b4525dc5af406da5e9722cf24165ccccb002
+
 
 
 // Adapter는 데이터를 RecyclerView에 주입하는 객체
--- app/src/main/java/com/example/mytableorder/fragment/home/ListAdapterHorizontal.kt
+++ app/src/main/java/com/example/mytableorder/fragment/home/ListAdapterHorizontal.kt
@@ -7,7 +7,9 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mytableorder.R
 
-class ListAdapterHorizontal(var list: ArrayList<String>):  RecyclerView.Adapter<ListAdapterHorizontal.ListAdapter>() {
+
+// 1. , , private val onItemClicked: (Int) -> Unit) 추가
+class ListAdapterHorizontal(var list: ArrayList<String>, private val onItemClicked: (Int) -> Unit):  RecyclerView.Adapter<ListAdapterHorizontal.ListAdapter>() {
 
     class ListAdapter(val layout: View): RecyclerView.ViewHolder(layout) {
         val textImg = layout.findViewById<TextView>(R.id.textImg)
@@ -18,8 +20,14 @@ class ListAdapterHorizontal(var list: ArrayList<String>):  RecyclerView.Adapter<
         return ListAdapter(view)
     }
 
+    //2.  holder.itemView.setOnClickListener {
+    //            onItemClicked(position)
+    //        }  클릭 리스너 추가
     override fun onBindViewHolder(holder: ListAdapter, position: Int) {
         holder.textImg.text = list[position]
+        holder.itemView.setOnClickListener {
+            onItemClicked(position)
+        }
     }
 
     override fun getItemCount(): Int {

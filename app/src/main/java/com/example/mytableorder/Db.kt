dcc6d1d0f6b0b6414cefcf61e3675af37e89f247
positiview <goaud2022@gmail.com>
2023-11-03 16:09:43 +0900

    커밋5

--- app/src/main/java/com/example/mytableorder/Db.kt
+++ app/src/main/java/com/example/mytableorder/Db.kt
@@ -1,12 +1,18 @@
 package com.example.mytableorder
 
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 
 object Db {
 
     var db : FirebaseFirestore = FirebaseFirestore.getInstance()
+    var rtdb : FirebaseDatabase = FirebaseDatabase.getInstance()
 
     fun getInstance() : FirebaseFirestore{
         return db
+    }
+
+    fun getRtDbInstance() :FirebaseDatabase{
+        return rtdb
     }
 }
